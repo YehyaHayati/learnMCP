67128d6b07948fa96d01af1761f355db3a253700
Yehya Hayati <yehya.hayati@systemsltd.com>
2025-08-18 16:33:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = MultiServerMCPClient({
-    "Stock_Search": {
-        "transport":"streamable_http",
-        "url":"http://localhost:8000/mcp/"
-    },
-    "Calculator": {
-        "transport":"streamable_http",
-        "url":"http://localhost:8001/mcp/"
+    # "Stock_Search": {
+    #     "transport":"streamable_http",
+    #     "url":"http://localhost:8000/mcp/"
+    # },
+    "Github": {
+        "transport": "streamable_http",
+        "url": "https://api.githubcopilot.com/mcp/"
     }
+    # "Calculator": {
+    #     "transport":"streamable_http",
+    #     "url":"http://localhost:8001/mcp/"
+    # }
 })
 
 async def main():
     tools = await client.get_tools()
     agent = create_react_agent(model=ChatOpenAI(), tools=tools, verbose=True)
-    question = "What is the stock of AAPL, tell me all of the information, and calculate the difference between High and Low."
+    # question = "What is the stock of AAPL, tell me all of the information, and calculate the difference between High and Low."
+    question = "Get me the readme of the repo MohammadYehya/GridForge."
     print(question)
     res = await agent.ainvoke({"messages":question})
     # for r in res['messages']:
